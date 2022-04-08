7416425a26c53b70292537fde1c7811e40c091c0
Max Savery <saverymax@gmail.com>
2022-04-08 14:39:54 +0200

    Add amsmath

--- doc/conf.py
+++ doc/conf.py
@@ -30,7 +30,7 @@ release = '1.0.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['myst_parser', 'sphinx.ext.mathjax']
+extensions = ['myst_parser', 'sphinx.ext.mathjax', 'amsmath']
 
 source_suffix = ['.rst', '.md']
 
